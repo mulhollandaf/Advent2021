fceaf2a3359d0755d3d33046230797c81b856495
John Mulholland <mulhollandj@gmail.com>
2021-12-25 18:26:59 -0700

    More of 25

--- src/day25/CucumberHelper.kt
+++ src/day25/CucumberHelper.kt
@@ -1,24 +1,79 @@
 package day25
 
 class CucumberHelper(input: List<CharArray>) {
-    val state = input
+    var state = input
+    val xMax = state[0].size - 1
+    val yMax = state.size - 1
     fun move(): List<CharArray> {
-        return state.mapIndexed {y, row ->
-            row.mapIndexed {x, cell ->
-                determineByNeighbors(x, y, state)
+        val first = turn(state, '>')
+        state = turn(first, 'v')
+        return state
+    }
+
+    private fun turn(map: List<CharArray>, which: Char): List<CharArray> =
+        map.mapIndexed { y, row ->
+            row.mapIndexed { x, cell ->
+                determineByNeighbors(map, x, y, which)
             }.toCharArray()
+        }
+
+    private fun determineByNeighbors(map: List<CharArray>, x: Int, y: Int, which: Char): Char {
+        return if (which == '>') {
+            if (leftMoveIntoThis(map, x, y)) {
+                '>'
+            } else if (thisMoveIntoRight(map,x, y)) {
+                '.'
+            } else {
+                map[y][x]
+            }
+        } else {
+            if (topMoveIntoThis(map,x, y)) {
+                'v'
+            } else if (thisMoveIntoBottom(map,x, y)) {
+                '.'
+            } else {
+                map[y][x]
+            }
         }
     }
 
-    private fun determineByNeighbors(x: Int, y: Int, map: List<CharArray>): Char {
-        return if (map[y][x] != '.') {
-            map[y][x]
-        }
-        else if (x > 0 && map[y][x - 1] == '>') {
-            '>'
-        } else {
-            '.'
-        }
+    private fun thisMoveIntoBottom(map: List<CharArray>, x: Int, y: Int): Boolean {
+        val spaceToTheBottom = if (y == yMax) map[0][x] else map[y+1][x]
+
+        return (map[y][x] == 'v' && spaceToTheBottom == '.')
     }
+
+    private fun thisMoveIntoRight(map: List<CharArray>, x: Int, y: Int): Boolean {
+        val spaceToTheRight = if (x == xMax) map[y][0] else map[y][x+1]
+
+        return (map[y][x] == '>' && spaceToTheRight == '.')
+    }
+
+    private fun leftMoveIntoThis(map: List<CharArray>, x: Int, y: Int): Boolean {
+        return (x > 0 && map[y][x - 1] == '>' && map[y][x] == '.') ||
+                (x == 0 && map[y][xMax] == '>' && map[y][x] == '.')
+    }
+
+    private fun topMoveIntoThis(map: List<CharArray>, x: Int, y: Int): Boolean {
+        return (y > 0 && map[y - 1][x] == 'v' && map[y][x] == '.') ||
+                (y == 0 && map[yMax][x] == 'v' && map[y][x] == '.')
+    }
+
+    fun runUntilStop(): Int {
+        var nTurns = 0
+        var end = state
+        while (nTurns < 10000) {
+            nTurns++
+            val start = end
+            val end = start.map {
+                it.toString().toCharArray()
+            }
+            if (start == end) {
+                return nTurns
+            }
+        }
+        return 0
+    }
+
 
 }
--- src/day25/Day25UnitTests.kt
+++ src/day25/Day25UnitTests.kt
@@ -14,12 +14,136 @@ class Day25UnitTests {
     }
 
     @Test
+    fun testOneMoveEastWrap() {
+        val input = convertToChars(listOf("..>"))
+        val helper = CucumberHelper(input)
+        val result = helper.move()
+        assertEquals(listOf(">.."), result.toListString())
+
+    }
+
+    @Test
     fun testMultiMoveEast() {
         val input = convertToChars(listOf(".>>"))
         val helper = CucumberHelper(input)
         val result = helper.move()
-        assertEquals(listOf(".>."), result.toListString())
+        assertEquals(listOf(">>."), result.toListString())
 
+    }
+
+    @Test
+    fun testOneMoveSouth() {
+        val input = convertToChars(listOf(".v.", "..."))
+        val helper = CucumberHelper(input)
+        val result = helper.move()
+        assertEquals(listOf("...", ".v."), result.toListString())
+
+    }
+
+    @Test
+    fun testOneMoveSouthWrap() {
+        val input = convertToChars(listOf("...", ".v."))
+        val helper = CucumberHelper(input)
+        val result = helper.move()
+        assertEquals(listOf(".v.", "..."), result.toListString())
+
+    }
+
+    @Test
+    fun testMultiMoveSouth() {
+        val input = convertToChars(listOf(".v.", ".v.", "..."))
+        val helper = CucumberHelper(input)
+        val result = helper.move()
+        assertEquals(listOf(".v.", "...", ".v."), result.toListString())
+
+    }
+
+    @Test
+    fun testTry1() {
+        val input = convertToChars(listOf(".>v.v.", "....>."))
+        val helper = CucumberHelper(input)
+        val result = helper.move()
+        assertEquals(listOf(".>....", "..v.v>"), result.toListString())
+
+    }
+
+    @Test
+    fun testBig1() {
+        val input = convertToChars(listOf(
+            "...>...",
+            ".......",
+            "......>",
+            "v.....>",
+            "......>",
+            ".......",
+            "..vvv.."
+        )
+        )
+        val helper = CucumberHelper(input)
+        val result = helper.move()
+        assertEquals(listOf(
+            "..vv>..",
+            ".......",
+            ">......",
+            "v.....>",
+            ">......",
+            ".......",
+            "....v.."
+
+        ), result.toListString())
+
+    }
+
+    @Test
+    fun testBig2() {
+        val input = convertToChars(listOf(
+            "..vv>..",
+            ".......",
+            ">......",
+            "v.....>",
+            ">......",
+            ".......",
+            "....v.."
+        )
+        )
+        val helper = CucumberHelper(input)
+        val result = helper.move()
+        assertEquals(listOf(
+            "....v>.",
+            "..vv...",
+            ".>.....",
+            "......>",
+            "v>.....",
+            ".......",
+            "......."
+
+        ), result.toListString())
+
+    }
+
+    @Test
+    fun testBroken() {
+        val helper = CucumberHelper(convertToChars(listOf("v.....>", ".......")))
+        val result = helper.move()
+        assertEquals(listOf("......>", "v......"), result.toListString())
+    }
+
+    @Test
+    fun testBroken2() {
+        val helper = CucumberHelper(convertToChars(listOf(">>.", "v..")))
+        val result = helper.move()
+        assertEquals(listOf(">.>", "v.."), result.toListString())
+    }
+
+    @Test
+    fun testStepsUntilStopEmpty() {
+        val input = convertToChars(listOf(
+            "..."
+        )
+        )
+        val helper = CucumberHelper(input)
+        val stepsUntilStop = helper.runUntilStop()
+        assertEquals(1, stepsUntilStop)
     }
 
     private fun convertToChars(listOf: List<String>): List<CharArray> {
